3a7d6886fdb363b56b7cdff404bf043deec1d221
+ <rodarte_sarah@yahoo.com>
2022-11-27 13:19:58 -0700

    commit tree bec57efd74bac21e8952a0bfa856abeb81b3b48f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(destination) {
+  if (destination > hq) {
+    return destination - hq;
+  } else if (hq > destination) {
+    return hq - destination;
+  }
+}
+
+function distanceFromHqInFeet(destination) {
+  return distanceFromHqInBlocks(destination) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  if (end > start) {
+    return (end - start) * 264;
+  } else {
+    return (start - end) * 264;
+  }
+}
+
+function calculatesFarePrice (start, end) {
+    let distance = distanceTravelledInFeet(start, end)
+    if( distance = 400)
+    return 0 
+}
+// } else if ( start >= end > 400 && end)
